a898a1af76106741e3b62120979b11987e140680
Olegivaniuk <olegivaniuk@gmail.com>
2025-12-10 19:58:16 +0200

    2d TC

---
+++ tests/Test Case 2 - Login User with correct email and password.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("Login", () => {
+  const userEmail = "olegivaniuk123@gmail.com"; // Replace with a valid registered email
+  const userPassword = "123"; // Replace with a valid registered password
+  const userName = "Oleg"; // Replace with a valid registered username
+
+  test("Login with valid creds", async ({ page }) => {
+    await page.goto("http://automationexercise.com");
+    await expect(page).toHaveTitle(/Automation Exercise/);
+    await expect(page).toHaveURL("https://automationexercise.com/");
+    await page.locator('a[href="/login"]').click();
+    await expect(
+      page.getByRole("heading", { name: "Login to your account" })
+    ).toBeVisible();
+    await page.getByPlaceholder("Email Address").first().fill(userEmail);
+    await page.getByPlaceholder("Password").fill(userPassword);
+    await page.getByRole("button", { name: "Login" }).click();
+    await expect(page.getByText(`Logged in as ${userName}`)).toBeVisible();
+
+    //Delete account
+    await page.getByRole("link", { name: " Delete Account" }).click();
+    await expect(page.getByText("Account Deleted!")).toBeVisible();
+  });
+});
